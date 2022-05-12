40478eee400de1c3de6b75301a6e3cbdd245a01a
darnhen <98438718+darnhen@users.noreply.github.com>
2022-05-11 23:08:32 -0400

    yea

---
+++ exercises/main.js
@@ -0,0 +1,242 @@
+// this is where you write your javascript
+
+$(window).keydown(function(e){
+
+  var key = e.key;
+
+  console.log(key);;
+
+  if (key == "a") {
+    var image_src = "image_one_one.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#one').html(image);
+  }
+  else if (key == "b") {
+    var image_src = "image_two_one.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#two').html(image);
+  }
+
+  else if (key == "c") {
+    var image_src = "image_three_one.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#three').html(image);
+  }
+
+  else if (key == "d") {
+    var image_src = "image_four_one.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#four').html(image);
+  }
+
+  else if (key == "y") {
+    $(body).css("background-color", "green");
+  }
+
+});
+
+$(window).keydown(function(d){
+
+  var key = d.key;
+
+  console.log(key);;
+
+  if (key == "e") {
+    var image_src = "image_one_two.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#one').html(image);
+  }
+
+  else if (key == "f") {
+    var image_src = "image_two_two.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#two').html(image);
+  }
+
+  else if (key == "g") {
+    var image_src = "image_three_two.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#three').html(image);
+  }
+
+  else if (key == "h") {
+    var image_src = "image_four_two.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#four').html(image);
+  }
+
+  else if (key == "i") {
+    $(body).css("background-color", "yellow");
+  }
+
+});
+
+$(window).keydown(function(c){
+
+  var key = c.key;
+
+  console.log(key);;
+
+  if (key == "j") {
+    var image_src = "image_one_three.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#one').html(image);
+  }
+
+  else if (key == "k") {
+    var image_src = "image_two_three.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#two').html(image);
+  }
+
+  else if (key == "l") {
+    var image_src = "image_three_three.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#three').html(image);
+  }
+
+  else if (key == "m") {
+    var image_src = "image_four_three.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#four').html(image);
+  }
+
+  else if (key == "n") {
+    $(body).css("background-color", "yellow");
+  }
+
+});
+
+$(window).keydown(function(b){
+
+  var key = b.key;
+
+  console.log(key);;
+
+  if (key == "o") {
+    var image_src = "image_one_four.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#one').html(image);
+  }
+
+  else if (key == "p") {
+    var image_src = "image_two_four.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#three').html(image);
+  }
+
+  else if (key == "q") {
+    var image_src = "image_three_four.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#three').html(image);
+  }
+
+  else if (key == "r") {
+    var image_src = "image_four_four.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#four').html(image);
+  }
+
+  else if (key == "s") {
+    $(body).css("background-color", "yellow");
+  }
+
+});
+
+$(window).keydown(function(a){
+
+  var key = a.key;
+
+  console.log(key);;
+
+  if (key == "t") {
+    var image_src = "image_six_one.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#one').html(image);
+  }
+
+  else if (key == "u") {
+    var image_src = "image_six_two.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#two').html(image);
+  }
+
+  else if (key == "v") {
+    var image_src = "image_six_three.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#three').html(image);
+  }
+
+  else if (key == "w") {
+    var image_src = "image_six_four.png";
+    var image = document.createElement("img");
+    image.src = image_src;
+    image.style.width = '100%';
+    image.style.height = '100%';
+    $('#four').html(image);
+  }
+
+  else if (key == "x") {
+    $(body).css("background-color", "yellow");
+  }
+
+});
+
+
